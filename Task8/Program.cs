bcd29344ec64a9b0d85ccedf8eac726519655ec8
Olga <laki_nebo@rambler.ru>
2022-06-27 01:41:17 +0300

    added Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number1 = 2;
+while (number1 <= number)
+{
+    Console.WriteLine(number1);
+    number1 = number1 + 2;
+}
