3a6cfaa2d483333d227d67e57ca075d47d03d61b
Olga <laki_nebo@rambler.ru>
2022-07-02 18:21:22 +0300

    added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int showNum() 
+{
+    int secondNum = num/10%10;
+    return secondNum;
+    
+}
+
+Console.WriteLine(showNum());
