a494b78f1a4a313c04956358af32bb561a96d561
Olga <laki_nebo@rambler.ru>
2022-07-23 20:45:34 +0300

    added Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] getArray(int mLocal, int nLocal)
+{
+    int[,] result = new int[mLocal, nLocal];
+    for (int i = 0; i < mLocal; i++)
+    {
+        for (int j = 0; j < nLocal; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void printArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void sortArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(1) - 1; k++)
+            {
+                if (inArray[i,k] < inArray[i,k+1])
+                {
+                    int temp = inArray[i,k+1];
+                    inArray[i,k+1] = inArray[i,k];
+                    inArray[i,k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int rows = 3;
+int columns = 4;
+int[,] array1 = getArray(rows, columns);
+printArray(array1);
+sortArray(array1);
+Console.WriteLine();
+printArray(array1);
