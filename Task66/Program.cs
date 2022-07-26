bb038fbff5dc58317880750275b988f86e661383
Olga <laki_nebo@rambler.ru>
2022-07-26 22:14:38 +0300

    added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetResult (int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + GetResult(m + 1 , n);
+    }
+}
+Console.WriteLine(GetResult(1, 15));
